c79a56a3e456d536766dcad4f2301e17fa06fd01
antoniafarias <antoniakalev19@gmail.com>
2026-04-02 19:42:14 -0300

    Corrige nome do módulo para infoEduca

--- infoEduca/infoEduca/wsgi.py
+++ infoEduca/infoEduca/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'InfoEduca.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'infoEduca.settings')
 
 application = get_wsgi_application()
